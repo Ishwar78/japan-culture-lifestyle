2f600dce4843b9fe9b790072505035aea15cedbb
Ishwar78 <157883619+Ishwar78@users.noreply.github.com>
2024-04-28 16:27:37 +0530

    Add files via upload

---
+++ inters.js
@@ -0,0 +1,25 @@
+
+function myFunction() {
+    document.getElementById("history").innerHTML = "It belongs to the Japonic language family."
+    
+    
+  }
+  if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+
+  let person = prompt("Please enter your name", "Sharma");
+let text;
+if (person == null || person == "") {
+  text = "User cancelled the prompt.";
+} else {
+  text = "Hello " + person + "! How are you today?";
+}
+window.alert("Are you sure to open this site ")
+
+
+
+
+
